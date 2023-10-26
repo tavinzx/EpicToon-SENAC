1196ebbf53be32a6950aa218a75c774138ec70c8
tavinzx <ustravo2@gmail.com>
2023-10-25 21:09:52 -0300

    finish

---
+++ assets/js/concluir.js
@@ -0,0 +1,14 @@
+function concluir(id){
+    // buscar do localstorage
+    const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+    // buscar a tarefa com o id
+    let tarefa = tarefas.find(t => t.id === id)
+
+    // alterar o estado da tarefa 
+    tarefa.concluida = true
+
+    // salvar no localstorage
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+    
+    atualiazar()
+}
